4ffec5cfc1e5ffa00935d0ce4f78063a604ccfae
shakirzue <99166577+shakirzue@users.noreply.github.com>
2022-04-20 02:07:47 -0400

    delete existing records then start fetching storing
    delete existing records from table then start fetch and store process. Display warning message and message related to how many records inserted and status of process

--- src/Pages/CMSApi/DMEGeography.js
+++ src/Pages/CMSApi/DMEGeography.js
@@ -12,6 +12,8 @@ function App() {
   const [data,setData] = useState([]);
   const [drawers, setDrawer] = useState("");
   const [size, setSize] = useState(5000);
+  const [rowInserted, setRowInserted] = useState(0);
+  const [fetchingFinishedLabel, setFetchingFinishedLabel] = useState("");
   const rootReducer = combineReducers({
     authorised2
 });
@@ -22,64 +24,66 @@ function App() {
     var rowsReturned=0;
     var offset = 0;
     var fetchingFinished = 0;
-    do {
-      console.log(size, offset);
-                    
-      await fetch("https://data.cms.gov/data-api/v1/dataset/bcd4a866-c07f-4ec3-a780-1fa7f0399ab9/data?size="+size+"&offset="+offset, 
-      {
-        method: "GET",
-        headers: {
-            "accept": "application/json"
+
+      await fetch(
+            process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DmeDeleteExistingData', {
+            method: 'POST',            
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
+                'oid': cookies.get('oid')
+            },
+            body: JSON.stringify({tableName: 'DME_Geography'})
+      })
+      .then(async ()=>{
+        do {
+          console.log(size, offset);
+          await fetch("https://data.cms.gov/data-api/v1/dataset/bcd4a866-c07f-4ec3-a780-1fa7f0399ab9/data?size="+size+"&offset="+offset, 
+          {
+            method: "GET",
+            headers: {
+                "accept": "application/json"
+            }
+          })
+          .then(async (response) => 
+            response.json()       
+          )
+          .then(async (response) => {
+            setFetchingFinishedLabel("");
+            rowsReturned = response.length;
+            if(rowsReturned < size)
+              fetchingFinished = 1;        
+            
+            await fetch(
+                  process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DMEGeographyDataStorage', {
+                  method: 'POST',            
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
+                      'oid': cookies.get('oid')
+                  },
+                  body: JSON.stringify(response)
+            })
+            .then(async (response) => 
+              response.json()       
+            )
+            .then(async (response) => {
+              if(response.success)
+                {
+                  offset = (offset+5000);
+                  setRowInserted(offset);
+                }
+              else{
+                fetchingFinished = 1;
+                alert("unable to store record : "+ response.message);
+              }
+            })
+          })
+        } while (!fetchingFinished)
+        if(fetchingFinished){
+          setFetchingFinishedLabel("Storing process finished. You can leave this page.");
         }
       })
-      .then(async (response) => 
-        response.json()       
-      )
-      .then(async (response) => {
-        console.log(response.length)
-        rowsReturned = response.length;
-        if(rowsReturned < size)
-          fetchingFinished = 1;        
-        
-        // if(response.length > 0 && offset === 0)
-        // {
-        //   await fetch(
-        //     process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DmeDeleteExistingData', {
-        //     method: 'POST',            
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
-        //         'oid': cookies.get('oid')
-        //     },
-        //     body: JSON.stringify({tableName: 'DME_Geography'})
-        //   })
-        // }
-        
-        await fetch(
-              process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DMEGeographyDataStorage', {
-              method: 'POST',            
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
-                  'oid': cookies.get('oid')
-              },
-              body: JSON.stringify(response)
-        })
-        .then(async (response) => 
-          response.json()       
-        )
-        .then(async (response) => {
-          if(response.success)
-            {
-              offset = (offset+5000);
-            }
-          else{
-            fetchingFinished = 1;
-            alert("unable to store record : "+ response.message);
-          }
-        })
-      })
-    } while (!fetchingFinished)
 }, [])
 const Panel = (props) => {
   return (
@@ -99,7 +103,9 @@ const Panel = (props) => {
                    
                    
                     <div className="emdeb-responsive">
-                      
+                      <h1>Total rows stored in db: {rowInserted}</h1>
+                      <h1>{fetchingFinishedLabel}</h1>
+                      <h2>Warning: Please do not close or refresh page unless finish message appear</h2>
                     </div>
                 </div>
             </Panel>
--- src/Pages/CMSApi/DMEService.js
+++ src/Pages/CMSApi/DMEService.js
@@ -12,6 +12,8 @@ function App() {
   const [data,setData] = useState([]);
   const [drawers, setDrawer] = useState("");
   const [size, setSize] = useState(5000);
+  const [rowInserted, setRowInserted] = useState(0);
+  const [fetchingFinishedLabel, setFetchingFinishedLabel] = useState("");
   const rootReducer = combineReducers({
     authorised
   });
@@ -22,62 +24,66 @@ function App() {
     var rowsReturned=0;
     var offset = 0;
     var fetchingFinished = 0;
-    do {
-        console.log(size, offset);
-        await fetch("https://data.cms.gov/data-api/v1/dataset/d6d3de93-0579-408a-bcfe-c319f04069e7/data?size="+size+"&offset="+offset, 
-        {
-          method: "GET",
-          headers: {
-              "accept": "application/json"
-          }
-        })
-        .then(async (response) => 
-          response.json()
-        )
-        .then(async (response) => {           
-            rowsReturned = response.length;
-            if(rowsReturned < size)
-              fetchingFinished = 1;
-           
-            // if(response.length > 0 && offset === 0)
-            // {
-            //   await fetch(
-            //       process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DmeDeleteExistingData', {
-            //       method: 'POST',            
-            //       headers: {
-            //           'Content-Type': 'application/json',
-            //           'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
-            //           'oid': cookies.get('oid')
-            //       },
-            //       body: JSON.stringify({tableName: 'DME_Service'})
-            //   })
-            // }
 
-            await fetch(
-                process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DMEServiceDataStorage', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
-                    'oid': cookies.get('oid')
-                },
-                body: JSON.stringify(response)
+    await fetch(
+                  process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DmeDeleteExistingData', {
+                  method: 'POST',            
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
+                      'oid': cookies.get('oid')
+                  },
+                  body: JSON.stringify({tableName: 'DME_Service'})
+    })
+   .then(async ()=>{
+        do {
+            console.log(size, offset);
+            await fetch("https://data.cms.gov/data-api/v1/dataset/d6d3de93-0579-408a-bcfe-c319f04069e7/data?size="+size+"&offset="+offset, 
+            {
+              method: "GET",
+              headers: {
+                  "accept": "application/json"
+              }
             })
             .then(async (response) => 
-                response.json()       
+              response.json()
             )
             .then(async (response) => {
-                if(response.success)
-                {
-                  offset = (offset+5000);
-                }
-                else{
-                  return;
-                  alert("unable to store record : "+ response.message);                  
-                }
-          })
-        })
-    } while (!fetchingFinished)
+              setFetchingFinishedLabel("");
+              rowsReturned = response.length;
+              if(rowsReturned < size)
+                fetchingFinished = 1;
+                              
+              await fetch(
+                    process.env.REACT_APP_SERVER_BASE_URL + 'cmsapi/DMEServiceDataStorage', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser'))?.token,
+                        'oid': cookies.get('oid')
+                    },
+                    body: JSON.stringify(response)
+              })
+              .then(async (response) => 
+                    response.json()       
+              )
+              .then(async (response) => {
+                    if(response.success)
+                    {
+                      offset = (offset+5000);
+                      setRowInserted(offset);
+                    }
+                    else{
+                      return;
+                      alert("unable to store record : "+ response.message);                  
+                    }
+              })
+            })
+        } while (!fetchingFinished)
+        if(fetchingFinished){
+          setFetchingFinishedLabel("Storing process finished. You can leave this page.");
+        }
+    });
   }, [])
 
 const Panel = (props) => {
@@ -96,7 +102,9 @@ const Panel = (props) => {
     <Panel value={1} index={1}>
         <div className="col-md-12">
             <div className="emdeb-responsive">
-            
+              <h1>Total rows stored in db: {rowInserted}</h1>
+              <h1>{fetchingFinishedLabel}</h1>
+              <h2>Warning: Please do not close or refresh page unless finish message appear</h2>
             </div>
         </div>
     </Panel>
